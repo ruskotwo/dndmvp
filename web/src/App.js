09aea7322bf69564fabe5047707430bea1314dfe
BVN4 <ruslan2804y@gmail.com>
2024-02-24 15:24:15 +0300

    Ws hotfix

--- web/src/App.js
+++ web/src/App.js
@@ -39,7 +39,7 @@ export default class App extends React.Component {
             return;
         }
 
-        this.ws = new WebSocket("wss://" + window.location.host + "/ws/game");
+        this.ws = new WebSocket("wss://" + window.location.host + ":4000/ws/game");
 
         this.ws.onopen = this.onWSOpen;
         this.ws.onclose = this.onWSClose;
